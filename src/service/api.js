6681ff119e87178030ed12bfcbf29a5e67bae898
Manglam Raj <124775042+manglamraj@users.noreply.github.com>
2023-08-18 16:15:43 +0530

    adding vercel link

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URI = 'http://localhost:8000';
+const API_URI = 'https://file-sharing-app-server-rust.vercel.app';
 
 export const uploadFile = async (data) => {
     try {
