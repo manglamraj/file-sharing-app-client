ed40a5c4bcea018d830f6cbe97e197de7057710d
Manglam Raj <124775042+manglamraj@users.noreply.github.com>
2023-08-18 20:07:59 +0530

    adding url link

--- src/service/api.js
+++ src/service/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
-
-const API_URI = 'https://file-sharing-app-server-beta.vercel.app';
-
+//   const API = `https://file-sharing-app-server-beta.vercel.app/upload`
 export const uploadFile = async (data) => {
     try {
-        const response = await axios.post(`${API_URI}/upload`, data);
+        const response = await axios.post("https://file-sharing-app-server-beta.vercel.app/upload", data);
+        console.log(response);
         return response.data;
     } catch (error) {
         console.log('Error while calling the API ', error.message);
